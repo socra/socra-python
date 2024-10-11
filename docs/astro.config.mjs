07f0ddf1e7148fce2a334a0a9222f70a912dc2d6
Mike <mdmorton6@gmail.com>
2024-10-11 18:39:20 -0500

    basic branding for docs site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,38 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import markdoc from '@astrojs/markdoc';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import markdoc from "@astrojs/markdoc";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://python.socra.com",
   integrations: [
-      markdoc(),
-      starlight({
-          title: 'My Docs',
-          social: {
-              github: 'https://github.com/withastro/starlight',
-          },
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
+    markdoc(),
+    starlight({
+      title: "socra Python",
+      social: {
+        github: "https://github.com/socra/socra-python",
+        "x.com": "https://x.com/socra_ai",
+      },
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      sidebar: [
+        "introduction",
+        // {
+        //   label: "Introduction",
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     // { label: "Example Guide", slug: "guides/example" },
+        //   ],
+        // },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
           ],
-      }),
-	],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 
-  output: 'server',
+  output: "server",
 
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
