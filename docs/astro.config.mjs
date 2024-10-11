16c049f6622e008675d3b8a145fc5eafe44e0ed1
Mike <mdmorton6@gmail.com>
2024-10-11 17:23:48 -0500

    add dockerfile and first attempt to deploy

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,28 +3,36 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import markdoc from '@astrojs/markdoc';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		markdoc(),
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      markdoc(),
+      starlight({
+          title: 'My Docs',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
